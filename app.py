1cb94e935dc3dd1378dab46dc50b2ca58b6be0dd
Rudransh Sharma <rudranshsharma2007@gmail.com>
2026-06-27 22:01:06 +0530

    Add FastAPI endpoint for website generation

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from fastapi import FASTAPI
-from agents.website_agent import WebsiteAgent
+from fastapi import FastAPI
 from schemas.website_schema import BusinessInput
+from agents.website_agent import WebsiteAgent,WebsiteOutput
 
-app=FASTAPI()
+app=FastAPI(title="GrowthPilot API",version="1.0.0")
 
-@app.post("/generate-website")
+@app.get("/")
+def home():
+    return {"message":"GrowthPilot Backend Running!"}
 
+@app.post("/generate-website",response_model=WebsiteOutput)
 def generate_website(business:BusinessInput):
 
     return WebsiteAgent.generate_website(business)
